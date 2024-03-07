230e2cea72325ebd16ec51c2e03c9ced9858c4ae
Ava Wandersleben <awandersleben05@gmail.com>
2024-03-07 13:13:54 -0500

    added storage and fragment for login

--- app/src/main/java/com/example/spotifyapp2340/MainActivity.java
+++ app/src/main/java/com/example/spotifyapp2340/MainActivity.java
@@ -1,9 +1,16 @@
 package com.example.spotifyapp2340;
 
+import android.nfc.Tag;
 import android.os.Bundle;
+import android.util.Log;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
@@ -11,14 +18,24 @@ import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
 import com.example.spotifyapp2340.databinding.ActivityMainBinding;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding binding;
+    private static FirebaseFirestore db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        db = FirebaseFirestore.getInstance();
 
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
@@ -32,6 +49,41 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(binding.navView, navController);
+    }
+
+    /**
+     * Please pass in a formatted string in the following way:
+     * "Name: [username]; [{}] (JSON OBJECTS OF SERIALIZED SPOTIFY WRAPPED)
+     * @param s formatted string
+     */
+    public static void newUser(String s) {
+        // Create a new user with a first and last name
+        CollectionReference usersWrapped = db.collection("users");
+        Map<String, String> user = new HashMap<>();
+        user.put("prior_wrapped", s.substring(s.indexOf("[")));
+        usersWrapped.document(s.substring(0, s.indexOf(";[{"))).set(user);
+    }
+
+    /**
+     * get prior wrapped in JSON format
+     * @param user
+     * @return
+     */
+    public static String getWrappedData(String user) {
+        DocumentReference docRef = db.collection("users").document(user);
+        final String[] returnStr = new String[1];
+        docRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                if (task.isSuccessful()) {
+                    DocumentSnapshot document = task.getResult();
+                    if (document.exists()) {
+                        returnStr[0] = (String) document.getData().get("prior_wrapped");
+                    }
+                }
+            }
+        });
+        throw new RuntimeException("Failed to retrieve past wrapped data");
     }
 
 }
