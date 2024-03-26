456faf6e6cd5296d98713af0ea96cfd829f851ea
Ava Wandersleben <awandersleben05@gmail.com>
2024-03-26 16:19:01 -0400

    Updated new user storage

--- app/src/main/java/com/example/spotifyapp2340/MainActivity.java
+++ app/src/main/java/com/example/spotifyapp2340/MainActivity.java
@@ -28,6 +28,7 @@ import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.io.IOException;
+import java.util.Calendar;
 import java.util.HashMap;
 import java.util.Map;
 import org.json.JSONObject;
@@ -73,6 +74,121 @@ public class MainActivity extends AppCompatActivity {
         NavigationUI.setupWithNavController(binding.navView, navController);
         User user = new User("User6", "Megan");
         newUser(user);
+        updateUser(user);
+        user.addWrapped(new Wrapped(Calendar.getInstance(), Calendar.getInstance(), "{\n" +
+                "  \"external_urls\": {\n" +
+                "    \"spotify\": \"string\"\n" +
+                "  },\n" +
+                "  \"followers\": {\n" +
+                "    \"href\": \"string\",\n" +
+                "    \"total\": 0\n" +
+                "  },\n" +
+                "  \"genres\": [\"Prog rock\", \"Grunge\"],\n" +
+                "  \"href\": \"string\",\n" +
+                "  \"id\": \"string\",\n" +
+                "  \"images\": [\n" +
+                "    {\n" +
+                "      \"url\": \"https://i.scdn.co/image/ab67616d00001e02ff9ca10b55ce82ae553c8228\",\n" +
+                "      \"height\": 300,\n" +
+                "      \"width\": 300\n" +
+                "    }\n" +
+                "  ],\n" +
+                "  \"name\": \"string\",\n" +
+                "  \"popularity\": 0,\n" +
+                "  \"type\": \"artist\",\n" +
+                "  \"uri\": \"string\"\n" +
+                "}", "{\n" +
+                "  \"album\": {\n" +
+                "    \"album_type\": \"compilation\",\n" +
+                "    \"total_tracks\": 9,\n" +
+                "    \"available_markets\": [\"CA\", \"BR\", \"IT\"],\n" +
+                "    \"external_urls\": {\n" +
+                "      \"spotify\": \"string\"\n" +
+                "    },\n" +
+                "    \"href\": \"string\",\n" +
+                "    \"id\": \"2up3OPMp9Tb4dAKM2erWXQ\",\n" +
+                "    \"images\": [\n" +
+                "      {\n" +
+                "        \"url\": \"https://i.scdn.co/image/ab67616d00001e02ff9ca10b55ce82ae553c8228\",\n" +
+                "        \"height\": 300,\n" +
+                "        \"width\": 300\n" +
+                "      }\n" +
+                "    ],\n" +
+                "    \"name\": \"string\",\n" +
+                "    \"release_date\": \"1981-12\",\n" +
+                "    \"release_date_precision\": \"year\",\n" +
+                "    \"restrictions\": {\n" +
+                "      \"reason\": \"market\"\n" +
+                "    },\n" +
+                "    \"type\": \"album\",\n" +
+                "    \"uri\": \"spotify:album:2up3OPMp9Tb4dAKM2erWXQ\",\n" +
+                "    \"artists\": [\n" +
+                "      {\n" +
+                "        \"external_urls\": {\n" +
+                "          \"spotify\": \"string\"\n" +
+                "        },\n" +
+                "        \"href\": \"string\",\n" +
+                "        \"id\": \"string\",\n" +
+                "        \"name\": \"string\",\n" +
+                "        \"type\": \"artist\",\n" +
+                "        \"uri\": \"string\"\n" +
+                "      }\n" +
+                "    ]\n" +
+                "  },\n" +
+                "  \"artists\": [\n" +
+                "    {\n" +
+                "      \"external_urls\": {\n" +
+                "        \"spotify\": \"string\"\n" +
+                "      },\n" +
+                "      \"followers\": {\n" +
+                "        \"href\": \"string\",\n" +
+                "        \"total\": 0\n" +
+                "      },\n" +
+                "      \"genres\": [\"Prog rock\", \"Grunge\"],\n" +
+                "      \"href\": \"string\",\n" +
+                "      \"id\": \"string\",\n" +
+                "      \"images\": [\n" +
+                "        {\n" +
+                "          \"url\": \"https://i.scdn.co/image/ab67616d00001e02ff9ca10b55ce82ae553c8228\",\n" +
+                "          \"height\": 300,\n" +
+                "          \"width\": 300\n" +
+                "        }\n" +
+                "      ],\n" +
+                "      \"name\": \"string\",\n" +
+                "      \"popularity\": 0,\n" +
+                "      \"type\": \"artist\",\n" +
+                "      \"uri\": \"string\"\n" +
+                "    }\n" +
+                "  ],\n" +
+                "  \"available_markets\": [\"string\"],\n" +
+                "  \"disc_number\": 0,\n" +
+                "  \"duration_ms\": 0,\n" +
+                "  \"explicit\": false,\n" +
+                "  \"external_ids\": {\n" +
+                "    \"isrc\": \"string\",\n" +
+                "    \"ean\": \"string\",\n" +
+                "    \"upc\": \"string\"\n" +
+                "  },\n" +
+                "  \"external_urls\": {\n" +
+                "    \"spotify\": \"string\"\n" +
+                "  },\n" +
+                "  \"href\": \"string\",\n" +
+                "  \"id\": \"string\",\n" +
+                "  \"is_playable\": false,\n" +
+                "  \"linked_from\": {\n" +
+                "  },\n" +
+                "  \"restrictions\": {\n" +
+                "    \"reason\": \"string\"\n" +
+                "  },\n" +
+                "  \"name\": \"string\",\n" +
+                "  \"popularity\": 0,\n" +
+                "  \"preview_url\": \"string\",\n" +
+                "  \"track_number\": 0,\n" +
+                "  \"type\": \"track\",\n" +
+                "  \"uri\": \"string\",\n" +
+                "  \"is_local\": false\n" +
+                "}"));
+        updateUser(user);
 
         //Task<Void> getWrapped = Tasks.whenAll(User.fetchTask);
 //        getWrapped.addOnSuccessListener(new OnSuccessListener<Void>() {
@@ -97,7 +213,7 @@ public class MainActivity extends AppCompatActivity {
         user.put("user_data", "");
 //        usersWrapped.document(s.substring(0, s.indexOf(";" + SPLITTER))).set(user);
         CollectionReference usersWrapped = db.collection("users");
-        usersWrapped.document(u.getId());
+        usersWrapped.document(u.getId()).set(user);
     }
 
     /**
