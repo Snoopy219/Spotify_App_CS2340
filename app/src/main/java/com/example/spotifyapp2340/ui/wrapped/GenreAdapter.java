452c0a759610bacc0d945a1fc6713d762f79c144
nikitara0 <nikitarao2005@gmail.com>
2024-04-09 12:57:57 -0400

    changed names in layout folder

--- app/src/main/java/com/example/spotifyapp2340/ui/wrapped/GenreAdapter.java
+++ app/src/main/java/com/example/spotifyapp2340/ui/wrapped/GenreAdapter.java
@@ -1,7 +1,6 @@
 package com.example.spotifyapp2340.ui.wrapped;
 
 import android.content.Context;
-import android.media.Image;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,8 +11,6 @@ import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.spotifyapp2340.R;
-import com.example.spotifyapp2340.wrappers.TrackObject;
-import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
 
